6f9c5a2a87caf446206885e697d98000dacc16d9
Chinmoy Barua <cbarua19@gmail.com>
2020-01-02 13:57:12 +0530

    Working With Strings

--- site.php
+++ site.php
@@ -10,11 +10,19 @@
 
 <body>
     <?php
-    $phrase = "To be or not to be";
-    $age = 30;
-    $gpa = 30.3;
-    $isMale = false;
+    $phrase = "Giraffe Academy";
+    echo strtolower($phrase);
+    echo "<br>";
+    echo strtoupper($phrase);
+    echo "<br>";
+    echo $phrase[0];
+    echo "<br>";
+    $phrase[0] = "B";
     echo $phrase;
+    echo "<br>";
+    echo str_replace("Biraffe", "Panda", $phrase);
+    echo "<br>";
+    echo substr($phrase, 8, 3);
     ?>
 </body>
 
